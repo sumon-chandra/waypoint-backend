5ccc2bb5c2eb4afcb8cf794e2c8eebeb8e44323e
Sumon Chandra <80484412+sumon-chandra@users.noreply.github.com>
2026-09-09 22:39:07 +0600

    refactor: protect users getting route

--- src/modules/users/users.route.ts
+++ src/modules/users/users.route.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
 import { UsersController } from "./users.controller";
+import { authGuard } from "../../middlewares/auth.middleware";
+import { UserRole } from "../../../types";
 
 const router = Router();
 
-router.get("/", UsersController.getAllUsers);
+router.get("/",
+  authGuard(UserRole.ADMIN),
+  UsersController.getAllUsers
+);
 
 export const UsersRoutes = router;
